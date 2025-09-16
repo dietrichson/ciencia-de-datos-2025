fcfa1fb26ba158935c5c4d791924d9672f5f90f0
aleksander dietrichson <dietrichson@gmail.com>
2025-09-16 22:59:34 +0000

    notas y tarea

--- R/poner-genero.R
+++ R/poner-genero.R
@@ -4,3 +4,10 @@ poner_genero <- function(mis_datos){
     rename(sexo=CH04) |> 
     mutate(sexo = ifelse(sexo==1,"Hombre","Mujer"))
 }
+
+poner_genero_y_edad <- function(mis_datos){
+  mis_datos |> 
+    rename(sexo=CH04) |> 
+    rename(edad=CH06) |> 
+    mutate(sexo = ifelse(sexo==1,"Hombre","Mujer"))
+}
